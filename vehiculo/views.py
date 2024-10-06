90edfd09272ad4eea20733b3ab6bf56e3de65269
Felipe Arellano <faaa1993@gmail.com>
2024-10-06 19:37:36 -0300

    commit_inicial

---
+++ vehiculo/views.py
@@ -0,0 +1,102 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth import authenticate, login
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from .models import User, Vehiculo
+
+from .forms import VehiculoForm
+from django.http import HttpResponseRedirect
+
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+
+
+# Create your views here.
+
+#INDEX-----------------------------------------------
+def index(request):
+    context = {}
+
+    return render(request, "index.html", context)
+#----------------------------------------------------
+
+#REGISTRO USUARIO------------------------------------
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'registration/register.html')
+
+    def post(self, request):
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 != password2:
+            messages.error(request, 'Passwords do not match')
+            return redirect(reverse('register'))  
+        user = User.objects.create_user(username=email, email=email, password=password1, first_name=first_name, last_name=last_name)
+        user.is_active = True
+        user.save()
+        
+        content_type = ContentType.objects.get_for_model(Vehiculo)
+        visualizar_catalogo = Permission.objects.get(codename='visualizar_catalogo', content_type=content_type )
+        # Agregamos el permiso al usuario
+        user.user_permissions.add(visualizar_catalogo)
+
+        user = authenticate(username=email, password=password1)
+        if user is not None:
+            login(request, user)
+        messages.success(request, 'Usuario creado exitosamente')
+        return redirect('index')
+    
+class CustomLoginView(SuccessMessageMixin, LoginView):
+    success_message = "Sesion Iniciada Exitosamente"
+    template_name = 'registration/login.html'  
+    redirect_authenticated_user = True
+    
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('login')
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        messages.add_message(request, messages.WARNING, "Sesion Cerrada Exitosamente")
+        return response
+#-----------------------------------------------------
+
+#FORMULARIO VEHICULOS---------------------------------
+@login_required
+def vehiculoform_view(request):
+    if not request.user.has_perm('vehiculo.add_vehiculo'):
+        messages.warning(request, 'No posees los permisos para acceder a este recurso!')
+        return HttpResponseRedirect('/')
+
+    if request.method == 'POST':
+        form = VehiculoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Vehículo ingresado exitosamente')
+            return HttpResponseRedirect('/')
+    else:
+        form = VehiculoForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'vehiculo_add.html', context)
+#-----------------------------------------------------
+
+#LISTA VIEHICULOS-------------------------------------
+@login_required
+def vehiculolist_view(request):
+    if not request.user.has_perm('vehiculo.visualizar_catalogo'):
+        messages.warning(request, 'No posees los permisos para acceder a este recurso!')
+        return HttpResponseRedirect('/')
+
+    sort_field = request.GET.get('sort', 'marca')  # Ordenar por 'marca' por defecto
+    lista_vehiculos = Vehiculo.objects.all().order_by(sort_field)
+    context = { "vehiculo_list": lista_vehiculos }
+    return render(request, "vehiculo_list.html", context)
